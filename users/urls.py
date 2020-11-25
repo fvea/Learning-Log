d6da226640949bea344b927eed2d8b07eb30f0e1
= <=>
2020-11-25 16:08:51 +0800

    Initial Commit: Ready for Remote Repo(Github)

---
+++ users/urls.py
@@ -0,0 +1,20 @@
+"""Define URL patterns for users."""
+
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+urlpatterns = [
+    # Login page
+    url(r'^login/$', LoginView.as_view(template_name='users/login.html'), 
+            name='login'),
+    # Logout
+    url(r'^logout/$', views.logout_view, name='logout'),
+
+    # Registration page
+    url(r'^register/$', views.register, name='register'),
+
+]
+
+app_name = 'users'
